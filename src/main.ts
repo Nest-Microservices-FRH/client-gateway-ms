1a0316d2ee008f153b6bdad2621a45255954c938
Fareh Romero <37407746+fareh150@users.noreply.github.com>
2025-01-13 15:12:30 +0100

    add logger and port

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Logger } from '@nestjs/common';
+import { envs } from './config';
 
-async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+async function bootstrap(): Promise<void> {
+    const logger = new Logger('Main-Gateway');
+
+    const app = await NestFactory.create(AppModule);
+    await app.listen(envs.port);
+
+    logger.log(`Server is running on port ${envs.port}`);
 }
 bootstrap();
