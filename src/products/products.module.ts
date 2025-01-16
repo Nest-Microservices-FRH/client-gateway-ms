f1e064ae8f62f44f54ec220341faab699e6843b7
Fareh Romero <37407746+fareh150@users.noreply.github.com>
2025-01-16 15:12:33 +0100

    start configuration to conect and creation of env data

--- src/products/products.module.ts
+++ src/products/products.module.ts
@@ -1,6 +1,8 @@
 import { Module } from '@nestjs/common';
 import { ProductsController } from './products.controller';
 import { ClientsModule, Transport } from '@nestjs/microservices';
+import { PRODUCT_SERVICE } from 'src/config/services';
+import { envs } from 'src/config';
 
 @Module({
     controllers: [ProductsController],
@@ -8,8 +10,12 @@ import { ClientsModule, Transport } from '@nestjs/microservices';
     imports    : [
         ClientsModule.register([
             {
-                name     : 'PRODUCT_SERVICE',
+                name     : PRODUCT_SERVICE,
                 transport: Transport.TCP,
+                options  : {
+                    host: envs.productsMicroserviceHost,
+                    port: envs.productsMicroservicePort,
+                },
             },
         ]),
     ],
