59efbb5e6b672b7223f7a88489589835e1db7f80
Fareh Romero <37407746+fareh150@users.noreply.github.com>
2025-01-16 14:15:16 +0100

    start change

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -1,7 +1,6 @@
 import { Controller } from '@nestjs/common';
-import { ProductsService } from './products.service';
 
 @Controller('products')
 export class ProductsController {
-  constructor(private readonly productsService: ProductsService) {}
+    constructor() {}
 }
