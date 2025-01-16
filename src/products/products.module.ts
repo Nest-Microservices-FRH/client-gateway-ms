c34fdf627ca36df8b4efec24f9613e7f19687ef0
Fareh Romero <37407746+fareh150@users.noreply.github.com>
2025-01-16 15:02:16 +0100

    start configuration microservices

--- src/products/products.module.ts
+++ src/products/products.module.ts
@@ -1,8 +1,17 @@
 import { Module } from '@nestjs/common';
 import { ProductsController } from './products.controller';
+import { ClientsModule, Transport } from '@nestjs/microservices';
 
 @Module({
     controllers: [ProductsController],
     providers  : [],
+    imports    : [
+        ClientsModule.register([
+            {
+                name     : 'PRODUCT_SERVICE',
+                transport: Transport.TCP,
+            },
+        ]),
+    ],
 })
 export class ProductsModule {}
