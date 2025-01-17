a310cd52a003a52c23102124e237fc09b792998c
Fareh Romero <37407746+fareh150@users.noreply.github.com>
2025-01-17 12:58:46 +0100

    custom error check example

--- src/common/exceptions/rpc-custom-exception.filter.ts
+++ src/common/exceptions/rpc-custom-exception.filter.ts
@@ -1,10 +1,27 @@
-import { ArgumentsHost, Catch, RpcExceptionFilter, UnauthorizedException } from '@nestjs/common';
+import { ArgumentsHost, Catch, ExceptionFilter } from '@nestjs/common';
 import { RpcException } from '@nestjs/microservices';
-import { Observable, throwError } from 'rxjs';
 
 @Catch(RpcException)
-export class RpcCustomExceptionFilter implements RpcExceptionFilter<RpcException> {
-    catch(exception: RpcException, host: ArgumentsHost): Observable<any> {
-        throw new UnauthorizedException('Algo por poner aqui');
+export class RpcCustomExceptionFilter implements ExceptionFilter {
+    catch(exception: RpcException, host: ArgumentsHost): any {
+        const ctx = host.switchToHttp();
+        const response =  ctx.getResponse();
+
+        const rpcError = exception.getError();
+
+        if (
+            typeof rpcError === 'object' &&
+            'status' in rpcError &&
+            'message' in rpcError
+        ) {
+            const status = isNaN(+rpcError.status) ? 400 : +rpcError.status;
+            return response.status(status).json(rpcError);
+        }
+        console.log('rpcError', rpcError);
+
+        response.status(400).json({
+            status : 400,
+            message: rpcError,
+        });
     }
 }
