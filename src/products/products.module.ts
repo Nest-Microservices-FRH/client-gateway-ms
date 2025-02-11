dd0ea4e6937f36d22f7cc741961efb9011dce5b2
Fareh Romero <37407746+fareh150@users.noreply.github.com>
2025-02-11 15:19:33 +0100

    use nats

--- src/products/products.module.ts
+++ src/products/products.module.ts
@@ -1,22 +1,12 @@
 import { Module } from '@nestjs/common';
 import { ProductsController } from './products.controller';
-import { ClientsModule, Transport } from '@nestjs/microservices';
-import { NATS_SERVICE } from 'src/config/services';
-import { envs } from 'src/config';
+import { NatsModule } from 'src/transports/nats.module';
 
 @Module({
     controllers: [ProductsController],
     providers  : [],
     imports    : [
-        ClientsModule.register([
-            {
-                name     : NATS_SERVICE,
-                transport: Transport.NATS,
-                options  : {
-                    servers: envs.natsServers,
-                },
-            },
-        ]),
+        NatsModule,
     ],
 })
 export class ProductsModule {}
